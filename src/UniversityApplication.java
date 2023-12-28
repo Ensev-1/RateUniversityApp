4a7217954e4addcb09969db65783d05bd68e5e36
ina-musta <Ina.Musta@fti.edu.al>
2023-12-28 15:50:00 +0100

    Creating the main page of the application.

--- src/UniversityApplication.java
+++ src/UniversityApplication.java
@@ -1,7 +1,14 @@
+import javax.swing.*;
+
 public class UniversityApplication {
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
 
+        FrameApplication f = new FrameApplication();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(450, 400);
+        f.setVisible(true);
 
     }
+
 }
