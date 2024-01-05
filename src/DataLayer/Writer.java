23134b57b59c5278c928368a24452a8805d4e3c2
eramata <eralda.mata@fti.edu.al>
2024-01-05 14:35:20 +0100

    Added method registerInCourse for registered Students in the Writer class.

--- src/DataLayer/Writer.java
+++ src/DataLayer/Writer.java
@@ -10,6 +10,13 @@ public class Writer implements Reader{
         String query="INSERT INTO feedbacks(FeedbackDescription, CourseID, FeedbackRating, UserID) VALUES('"+feedbackDescription+"','"+courseID+"','"+rating+"','"+userID+"')";
         stmt.executeUpdate(query);
 
+        public void registerInCourse(int userID, int courseID) throws SQLException {
+            Connection conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/UniversityApplication", "root", "");
+            Statement stmt = conn.createStatement();
+            String query="INSERT INTO coursesforusers(UserID, CourseID) VALUES('"+userID+"','"+courseID+"')";
+            stmt.executeUpdate(query);
+        }
+
     }
 
 
