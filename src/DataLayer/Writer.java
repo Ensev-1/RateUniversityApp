1ade835ae2c316115f4e51ce3b9fb27d2f1db3b5
ina-musta <Ina.Musta@fti.edu.al>
2024-01-05 13:13:03 +0100

    Addeded Writer class and a method to leaveFeedback.

---
+++ src/DataLayer/Writer.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+
+public class Writer implements Reader{
+    private Connection conn = null;
+
+    public void leaveFeedback(String feedbackDescription, int rating, int courseID, int userID) throws SQLException {
+
+        conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/UniversityApplication", "root", "");
+        Statement stmt = conn.createStatement();
+        String query="INSERT INTO feedbacks(FeedbackDescription, CourseID, FeedbackRating, UserID) VALUES('"+feedbackDescription+"','"+courseID+"','"+rating+"','"+userID+"')";
+        stmt.executeUpdate(query);
+
+    }
+
+
+}
